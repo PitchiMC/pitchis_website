70c7b6d08b22be92dd4d536c2556f5786be0aee9
PitchiMC <pitchiniko@gmail.com>
2026-07-04 23:01:19 +0200

    added modrinth logo to villi side (just for testing), updated villager on a leash side

--- projects/script.js
+++ projects/script.js
@@ -2,16 +2,13 @@ function copyConfig() {
     const codeElement = document.getElementById("configCode");
     const btn = document.getElementById("copyBtn");
     
-    // Text aus dem pre-Tag extrahieren
     const code = codeElement.innerText;
 
     navigator.clipboard.writeText(code).then(() => {
-        // Visuelles Feedback
         const originalText = btn.innerText;
         btn.innerText = "Copied!";
         btn.classList.add("copied");
 
-        // Nach 2 Sekunden zurücksetzen
         setTimeout(() => {
             btn.innerText = originalText;
             btn.classList.remove("copied");
